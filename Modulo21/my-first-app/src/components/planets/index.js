1451b5481456128113fc9a58b9045a6917b79c10
Alleson Henrique <alleson_henrique@hotmail.com>
2020-11-27 01:34:29 -0300

    Hooks, States e Lifecycle 1

--- Modulo21/my-first-app/src/components/planets/index.js
+++ Modulo21/my-first-app/src/components/planets/index.js
@@ -1,4 +1,4 @@
-import React, {Fragment} from 'react'
+import React, {Fragment, useState, useEffect} from 'react'
 import Planet from './planet'
 
 /*
@@ -20,70 +20,61 @@ async function getPlanets(){
     let data = await response.json()
     return data
 }
-
-class Planets extends React.Component{
-    constructor(props){
-        super(props)
-        this.state = {
-                planets: [
-
-
-                ]
-        }
-
-    }
+/*
+    
+*/
+const Planets = () =>{
+    const [planets, setPlanets] = useState([
+    ])
 
 
 
-    componentDidMount(){
-        getPlanets().then(data =>{
-            this.setState(state =>({
-                planets: data['planets']
-            }))
+    useEffect(() => {
+        getPlanets().then(data => {
+            setPlanets(data['planets'])
         })
+    }, [])
+    
 
-    }
 
-    removeLast = () =>{
-        let new_planets = [...this.state.planets]
+
+
+
+    const removeLast = () =>{
+        let new_planets = [...planets]
         new_planets.pop()
-        this.setState(state => ({
-            planets: new_planets //planets chama new planets
-
-        }))
+        setPlanets(new_planets)
 
     }
 
-    duplicateLastPlanet = () =>{
-        let last_planet = this.state.planets[this.state.planets.length - 1]//gerando uma chave que seleciona dentro de um array a posicao sendo [1,2,3].length - 1 que daria 2 
-        this.setState(state =>({
-            planets: [...this.state.planets, last_planet]//pega todos os planetas que existe e passa um novo com last Planet
-
-        }))
-
+    const duplicateLastPlanet = () =>{
+        let last_planet = planets[planets.length - 1]//gerando uma chave que seleciona dentro de um array a posicao sendo [1,2,3].length - 1 que daria 2 
+        setPlanets([...planets, last_planet])
     }
 
-    render() {
-        return (//o fragment eh usado para que consiga devolver o h3 e outras tags sem precisa criar outra div
-            <Fragment>
-                <h3>Planets list</h3>
-                {/*<button onClick={showMessage}>Show Message</button> */}
-                <button onClick={this.removeLast}>Remove Last</button>
-                <button onClick={this.duplicateLastPlanet}>Add Last Planet</button>
-                <hr />
-                {this.state.planets.map((planet) =>
-                    <Planet
-                        name={planet.name}
-                        description={planet.description}
-                        img_url={planet.img_url}
-                        link={planet.link}
-                        /*clickOnPlanet={clickOnPlanet}*/
-                        title_with_underline={planet.title_with_underline}
-                        grey={planet.grey}
-                        
-                    />
-                )}
-                {/*
+
+    return (//o fragment eh usado para que consiga devolver o h3 e outras tags sem precisa criar outra div
+        <Fragment>
+            <h3>Planets list</h3>
+            {/*<button onClick={showMessage}>Show Message</button> */}
+            <button onClick={removeLast}>Remove Last</button>
+            <button onClick={duplicateLastPlanet}>Add Last Planet</button>
+            <hr />
+            {planets.map((planet, index) =>
+                <Planet
+                    id={planet.id}
+                    name={planet.name}
+                    description={planet.description}
+                    img_url={planet.img_url}
+                    link={planet.link}
+                    /*clickOnPlanet={clickOnPlanet}*/
+                    title_with_underline={planet.title_with_underline}
+                    grey={planet.grey}
+                    key={index}
+
+                />
+            )}
+            {/*
                 <Planet
                     name={this.state.planets[1].name}
                     description={this.state.planets[1].description}
@@ -93,11 +84,11 @@ class Planets extends React.Component{
                 />
                 */}
 
-            </Fragment>
+        </Fragment>
 
 
-        )
-    }
+    )
+
 
     
     
--- Modulo21/my-first-app/src/components/planets/planet/index.js
+++ Modulo21/my-first-app/src/components/planets/planet/index.js
@@ -4,41 +4,82 @@ import DescriptionWithLink from "../../shared/description_with_link"
 
 
 
-const Planet = (props) =>{//Colocar uma funcao anonima passando pro onclick que chama a outra funcao {/* onClick={() =>props.clickOnPlanet(props.name)}*/} 
+
+async function getSatellites(id){
+    let response = await fetch(`http://localhost:3000/api/${id}.json`)
+    let data = await response.json()
+    return data
+}
+
+class Planet extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            satellites: []
+        }
+
+    }
+    componentDidMount(){
+        getSatellites(this.props.id).then(data => {
+            this.setState(state =>({
+                satellites: data["satellites"]
+            }))
+        })
+    }
+
+
+    
+    render(){
+        let title
+        if (this.props.title_with_underline)
+            title = <h4><u>{this.props.name}</u></h4>
+        else
+            title = <h4>{this.props.name}</h4>
+        return (
+            <div> 
+            
+                {title}
+                <DescriptionWithLink description={this.props.description} link={this.props.link}/>
+                <GrayImg img_url={this.props.img_url} gray={this.props.gray}/>
+                <h4>Satelites</h4>
+                <ul>
+                {this.state.satellites.map((satellites, index)=>
+                <li key={index}>{satellites.name}</li>
+                )}
+                </ul>
+                
+    
+                {/* <h4>Satelites</h4>
+                <ul>
+                    {
+                        ['a', 'b', 'c', 'd'].map((n) =>
+                            <li>Satelite {n}</li>
+                        )
+                    }
+                </ul>
+                */}
+                <hr/>
+            </div>
+                
+            
+        )
+
+    }
+}
+
+
+
+/*const Planet = (props) =>{//Colocar uma funcao anonima passando pro onclick que chama a outra funcao {/* onClick={() =>props.clickOnPlanet(props.name)}} 
 //o fragment eh usado para que consiga devolver o h3 e outras tags sem precisa criar outra div
 
 
     //const names = ['a', 'b', 'c', 'd']//nao abrir chaves aqui 
 
-    let title
-    if (props.title_with_underline)
-        title = <h4><u>{props.name}</u></h4>
-    else
-        title = <h4>{props.name}</h4>
+    
 
     
-    return (
-        <div> 
-        
-            {title}
-            <DescriptionWithLink description={props.description} link={props.link}/>
-            <GrayImg img_url={props.img_url} gray={props.gray}/>
+    
 
-            {/* <h4>Satelites</h4>
-            <ul>
-                {
-                    ['a', 'b', 'c', 'd'].map((n) =>
-                        <li>Satelite {n}</li>
-                    )
-                }
-            </ul>
-            */}
-            <hr/>
-        </div>
-            
-        
-    )
-
-}
+}*/
 
 export default Planet
