41d1313741a459a7c2d0d3dd5de8daa5ef576596
Alleson Henrique <alleson_henrique@hotmail.com>
2020-11-28 17:15:53 -0300

    Inicio do server 3000

--- Modulo22/terminal/file.js
+++ Modulo22/terminal/file.js
@@ -13,4 +13,4 @@ fs.appendFile('test.txt', 'Ola node js\n', err =>{
 //fs.rename('test.txt', 'test2.txt', err => console.log(err))
 fs.unlink('test2.txt', err => console.log(err))
 
-console.log(__dirname)
+//console.log(__dirname)
